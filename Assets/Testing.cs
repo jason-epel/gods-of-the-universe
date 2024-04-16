7afa97cbd41b7e9d8cb4e36771e9731ec5b97478
jason-epel <epelj1@gmail.com>
2024-04-16 15:18:24 -0500

    2024.04.16-am

--- Assets/Testing.cs
+++ Assets/Testing.cs
@@ -1,11 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CodeMonkey.Utils;
 
 public class Testing : MonoBehaviour {
 
-    private void Start() {
-        Grid grid = new Grid(20, 20, 10f);
-    }
+    private Grid grid;
 
+    private void Start() {
+        grid = new Grid(20, 20, 10f, new Vector3(0,0));
+        new Grid(2, 15, 10f, new Vector3(-40, 50));
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(), 56);
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            Debug.Log(grid.GetValue(UtilsClass.GetMouseWorldPosition()));
+        }
+    }
 }
